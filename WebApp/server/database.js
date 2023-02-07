498a6c3e270b47a3997bc76fb5a57037c7cb4232
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2023-02-07 11:52:09 -0500

    Created postgres Pool object for database

--- WebApp/server/database.js
+++ WebApp/server/database.js
@@ -1,6 +1,15 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE
+});
+
 /// Users
 
 /**
